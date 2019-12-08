d9496b22ced3e2e0288670b9a227b2c0444005de
Darrell Hanley <contact@darrellhanley.com>
2019-12-08 18:03:04 -0500

    initial commit

---
+++ src/views/app/app.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Chroma, { Color } from 'chroma-js';
+import Results from '../results/results';
+import { IAppState } from './types';
+
+export default class App extends Component {
+  state: IAppState = {
+    foreground: Chroma('black'),
+    background: Chroma('white')
+  }
+
+  /**
+   * Updates the foreground color
+   * @param foreground a Chroma color object
+   */
+  private setForeground = (foreground: Color) => this.setState({ foreground });
+
+  /**
+   * Updates the background color
+   * @param background a Chroma color object
+   */
+  private setBackground = (background: Color) => this.setState({ background });
+
+  private swapColors = () => this.setState({
+    foreground: this.state.background,
+    background: this.state.foreground
+  });
+
+  render() {
+    return (
+      <div>
+        <Results
+          foreground={this.state.foreground}
+          background={this.state.background}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/views/app/types.ts
@@ -0,0 +1,6 @@
+import { Color } from 'chroma-js';
+
+export interface IAppState {
+  foreground: Color;
+  background: Color;
+}
